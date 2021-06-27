bca692cba5480ce39b54d16395eda306430c2d1f
Shreesh Nayak <shreeshnayak@gmail.com>
2021-06-26 18:05:22 -0700

    Added vector icons

--- src/screens/Options.tsx
+++ src/screens/Options.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const styles = StyleSheet.create({
   row: {
     paddingHorizontal: 20,
     paddingVertical: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   text: {
     fontSize: 16,
@@ -22,18 +26,21 @@ const Options: React.FC = () => {
     <View>
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>Themes</Text>
+        <Icon name="chevron-right" size={20} color="#4F6D7A" />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basics</Text>
+        <Icon name="export" size={20} color="#4F6D7A" />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native by Example</Text>
+        <Icon name="export" size={20} color="#4F6D7A" />
       </TouchableOpacity>
     </View>
   );
